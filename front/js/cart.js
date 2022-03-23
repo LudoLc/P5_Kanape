7b24fb95382aa50433ec70168569adcfc37dbcad
LudoLc <95165233+LudoLc@users.noreply.github.com>
2022-03-23 15:37:17 +0100

    Product js done

---
+++ front/js/cart.js
@@ -0,0 +1,7 @@
+// const url = 'http://127.0.0.1:5500/front/html/cart.html'
+
+// fetch(url)
+// .then(response => response.json())
+// .then(response => {
+
+// })
--- front/js/product.js
+++ front/js/product.js
@@ -29,10 +29,10 @@ fetch(urlProduct)
     const select = document.getElementById('colors');
 
     product.colors.forEach((color) => { 
-        const optionsColor = document.createElement('option');
-        optionsColor.value = color;
-        optionsColor.textContent = color;
-        select.appendChild(optionsColor);
+            const optionsColor = document.createElement('option');
+            optionsColor.value = color;
+            optionsColor.textContent = color;
+            select.appendChild(optionsColor);
     })
     eventButton(product);
 })
@@ -43,29 +43,33 @@ fetch(urlProduct)
 function eventButton(product) {
     const button = document.getElementById('addToCart');
     if(button) {
-    button.addEventListener('click', (event) => {
-        event.preventDefault();
-        const quantity = document.getElementById('quantity').value;
-        const color = document.getElementById('colors').value;
-        // console.log('click');
-        // console.log(color);
-        // console.log(quantity);
-        if(color == '' || color == null || quantity == '' || quantity < 1 || quantity > 100) {
-            alert("Veuillez selectionner un produit ainsi qu'une couleur SVP ! ")
-        } else {
-        savingShop(product);
-        }
-    })
+        button.addEventListener("click", (event) => {
+            event.preventDefault();
+            const quantity = document.getElementById('quantity').value;
+            const color = document.getElementById('colors').value;
+            //console.log(event);
+            // console.log(color);
+            // console.log(quantity);
+            if(color == '' || color == null || quantity == '' || quantity < 1 || quantity > 100) {
+                alert("Veuillez selectionner un produit ainsi qu'une couleur SVP ! ")
+            } else {
+                savingShop(product);
+                const wantRedicrect = window.confirm("Avez-vous fini votre achat ? Si non, vous pouvez continuer votre visite sur le site. Votre panier sera sauvegardé. ");
+                if(wantRedicrect === true) {
+                    window.location.href = "cart.html";
+                }
             }
+        })
+    }
 }
 // verifie si le panier existe deja 
 function ifExist(array, value) {
     for (let elem of array) {
-        if (elem.name === value) {
+        if (elem.id === value.id && elem.color === value.color) {
             return true
         }
     }
-            return false
+    return false
 };
 
 
@@ -75,28 +79,29 @@ function savingShop(product) {
     // stockage : id / couleur / quantité
     const quantity = document.getElementById('quantity').value;
     const color = document.getElementById('colors').value;
+    console.log(product._id);
     const productCart = {
-        id: product.id,
-        name: product.name + ' ' + color,
+        id: product._id,
         quantity: parseInt(quantity),
-        color: color
+        color: color,
+        name: product.name
     }
     
-    let panier = localStorage.getItem('panier');
-    if(!panier) {
-        panier = [productCart];
+let panier = localStorage.getItem('panier');
+if(!panier) {
+    panier = [productCart];
     } else {
-        panier = JSON.parse(panier)
-            if(ifExist(panier, productCart.name)){
-                console.log('test')
-                const index = panier.findIndex(function (i) { //cherche l'index du produit dans le panier
-                    return i.name == productCart.name;
-                });
-                panier[index].quantity += parseInt(quantity)
-            } else {
-                panier.push(productCart)
-            }
-    }
+            panier = JSON.parse(panier)
+                if(ifExist(panier, productCart)){
+                    console.log('test')
+                    const index = panier.findIndex(function (i) { //cherche l'index du produit dans le panier
+                        return i.name == productCart.name;
+                    });
+                    panier[index].quantity += parseInt(quantity)
+                } else {
+                    panier.push(productCart)
+                }
+        }
     
     localStorage.setItem('panier', JSON.stringify(panier));
 }
